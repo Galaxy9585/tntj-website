f157ffbb1171a4ffe602beed1b1c5e641a9aeae7
Galaxy9585 <galaxy.nkm@gmail.com>
2025-08-06 20:23:57 +0530

    Updated package.json

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Tamil Nadu Thowheed Jamaath</h1>
+    <p class="read-the-docs">
+      Welcome to the TNTJ website
+    </p>
+  </div>
+`
